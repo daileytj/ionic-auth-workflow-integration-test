43817e2d1d2b90f014853538d2c450c8f7bd1845
daileytj <daileytj89@gmail.com>
2021-02-03 13:24:09 -0500

    Add auth workflow and start integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { getAuthSubRoutes, AUTH_ROUTES, PxbAuthGuard } from '@pxblue/angular-auth-workflow';
+import { AuthPage } from './auth/auth.page';
+import { HomePage } from './home/home.page';
 
+const authWorkflowRoutes = getAuthSubRoutes();
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+    { path: '', redirectTo: AUTH_ROUTES.AUTH_WORKFLOW, pathMatch: 'full' },
+    { path: AUTH_ROUTES.AUTH_WORKFLOW, component: AuthPage, children: authWorkflowRoutes },
+    {
+        path: '',
+        canActivate: [PxbAuthGuard],
+        children: [
+            { path: 'home', component: HomePage }
+        ],
+    },
+  // {
+  //   path: 'auth',
+  //   loadChildren: (): any => import('./auth/auth.module').then((m: any) => m.AuthPageModule)
+  // },
+  // {
+  //   path: 'home',
+  //   loadChildren: (): any => import('./home/home.module').then((m: any) => m.HomePageModule)
+  // },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PxbAuthModule, PxbAuthUIService, PxbRegisterUIService } from '@pxblue/angular-auth-workflow';
+import { AuthUIService } from './services/auth-ui.service';
+import { RegisterUIService } from './services/register-ui.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PxbAuthModule],
+  providers: [ {
+    provide: 'APP_NAME',
+    useValue: 'IONIC_TEST_AUTH_WORKFLOW_INTEGRATION',
+},
+{
+    provide: PxbAuthUIService,
+    useClass: AuthUIService,
+},
+{
+    provide: PxbRegisterUIService,
+    useClass: RegisterUIService,
+},],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
