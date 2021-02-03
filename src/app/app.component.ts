9996226981bc119ecd20c85d9c35748059e5f59f
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-02-03 14:18:06 -0500

    Add the secret sauce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {LocalStorageService} from "./services/localStorage.service";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(localStorageService: LocalStorageService, router: Router) {
+    localStorageService.listenForAuthLoadingStateChanges();
+    router.events.subscribe((route) => {
+      if (route instanceof NavigationEnd) {
+        console.log(route);
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { PxbAuthModule, PxbAuthUIService, PxbRegisterUIService } from '@pxblue/angular-auth-workflow';
 import { AuthUIService } from './services/auth-ui.service';
 import { RegisterUIService } from './services/register-ui.service';
+import {AuthPageModule} from "./auth/auth.module";
+import {HomePageModule} from "./home/home.module";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PxbAuthModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PxbAuthModule, AuthPageModule, HomePageModule],
   providers: [ {
     provide: 'APP_NAME',
     useValue: 'IONIC_TEST_AUTH_WORKFLOW_INTEGRATION',
@@ -24,7 +26,7 @@ import { RegisterUIService } from './services/register-ui.service';
 {
     provide: PxbRegisterUIService,
     useClass: RegisterUIService,
-},],
+}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
